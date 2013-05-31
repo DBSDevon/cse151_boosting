9b38cd691162b1af59d6e20cf435458abbe6c8ad
Bryan Ransil <bryan.ransil@gmail.com>
2013-05-31 15:36:38 -0700

    Outlining for the main function

--- code/boostAlgo.py
+++ code/boostAlgo.py
@@ -1,1 +1,21 @@
 def boosting():
+
+
+
+
+
+if __name__ == "__main__" :
+	# 1. read in the training file
+	# 2. create any persistent structures
+	# List of tuples (feature number, h1 or h0 type)
+	# 
+	# 3. for t loops:
+		# A. Run our boost algorithm
+		# B. Add the resulting tuple to our list
+	# 4. Read the test file
+	# 5. Create a variable for the total error and the total number of emails
+	# 6. For each email in the test file
+		# A. Label it according to our algorithm
+		# B. Compare the labels, add to the error count if it fails
+
+	# 7. Spit out the number for t and a fraction of error count / email count
