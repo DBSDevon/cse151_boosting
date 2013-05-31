1cf71fb4873390945a3fc5907419f2180af58e28
Bryan Ransil <bryan.ransil@gmail.com>
2013-05-31 15:48:20 -0700

    Adjustment for the weighted values

--- code/boostAlgo.py
+++ code/boostAlgo.py
@@ -8,10 +8,11 @@ if __name__ == "__main__" :
 	# 1. read in the training file
 	# 2. create any persistent structures
 	# List of tuples (feature number, h1 or h0 type)
-	# 
+	# Vector of weights
 	# 3. for t loops:
-		# A. Run our boost algorithm
+		# A. Run our boost algorithm (returns a tuple for our list and the new weights)
 		# B. Add the resulting tuple to our list
+		# C. Replace our current vector of weights with the new vector
 	# 4. Read the test file
 	# 5. Create a variable for the total error and the total number of emails
 	# 6. For each email in the test file
