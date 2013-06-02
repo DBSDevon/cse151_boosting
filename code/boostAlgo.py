84407f16693fe53320e4f6bdc921081c3ac22b70
Bryan Ransil <bryan.ransil@gmail.com>
2013-06-02 14:54:01 -0700

    Half of the boosting algorithm

--- code/boostAlgo.py
+++ code/boostAlgo.py
@@ -1,14 +1,33 @@
+weights
+'''
+with the global dataset and global weights
+'''
 def boosting():
+	#1. Declare the global dataset and weight, but they have been previously initialized
+	global weights
+	bestFeature = -1
+	totalError = 100.0
 
+	#2. For each feature in a data point
+	for ( i in range(0, weights.length) ) :
+		# A. calculate the total error via evaluating each datapoint
+		error = 0.0
+		# B. If the total error is less than previous best error, then replace the current error with this and the current feature record
+		if (error < totalError) :
+			bestFeature = i
+			totalError = error
 
-
+	#3. Get the error information
+	
+	#4. change the weights
+	
 
 
 if __name__ == "__main__" :
 	# 1. read in the training file
 	# 2. create any persistent structures
 	# List of tuples (feature number, h1 or h0 type)
-	# Vector of weights
+	# global Vector of weights
 	# 3. for t loops:
 		# A. Run our boost algorithm (returns a tuple for our list and the new weights)
 		# B. Add the resulting tuple to our list
